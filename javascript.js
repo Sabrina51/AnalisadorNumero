83b48f383c325e016ec5f54ffb5ad34cab70f228
sabrina51 <sabrina.silva51@fatec.sp.gov.br>
2021-09-04 23:32:38 -0300

    add rules to clear and focus number add field

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 var domElementoNumero = document.getElementById('numero');
+var domElementoDivResultado = document.getElementById('resultados');
 var valorNumero = [];
 
 function botao_adicionar() {
@@ -12,40 +13,52 @@ function botao_adicionar() {
             valorNumero.push(numero);
             let domElementoAdicionados = document.getElementById('adicionados');
             let option = document.createElement("option");
-            option.append(`O valor ${numero} foi adicionado!`);
-            domElementoAdicionados.append(option);
+            option.text = `O valor ${numero} foi adicionado!`;
+            domElementoAdicionados.appendChild(option);
+            //option.append(`O valor ${numero} foi adicionado!`);
+            //domElementoAdicionados.append(option);
+            domElementoDivResultado.innerHTML = '';
         }
     }
+    domElementoNumero.value = '';
+    domElementoNumero.focus();
 }
 
 function botao_finalizar() {
-    let domElementoDivResultado = document.getElementById('resultados');
+    //var domElementoDivResultado = document.getElementById('resultados');
     let quantidade = Number(valorNumero.length);
     let maior = Math.max.apply(null, valorNumero);
     let menor = Math.min.apply(null, valorNumero);
     let soma = 0;
-    
+
     for (let i = 0; i < valorNumero.length; i++) {
         soma = Number(soma) + Number(valorNumero[i]);
     }
 
     var media = soma / quantidade;
-    
-    let paragrafoResultado = document.createElement("p");
 
-    paragrafoResultado.append(`Ao todo, temos ${quantidade} números cadastrados.`);
-    domElementoDivResultado.append(paragrafoResultado);
+    if (valorNumero.length === 0) {
+        alert('[ERRO] Faltam valores. Impossível calcular!')
+    } else {
+        let paragrafoResultado = document.createElement("p");
+        domElementoDivResultado.innerHTML = '';
 
-    paragrafoResultado.append(`O maior valor informado foi ${maior}.`);
-    domElementoDivResultado.append(paragrafoResultado);
+        paragrafoResultado.innerHTML = `<p>Ao todo, temos ${quantidade} números cadastrados.</p>`;
+        domElementoDivResultado.appendChild(paragrafoResultado);
 
-    paragrafoResultado.append(`O menor valor informado foi ${menor}.`);
-    domElementoDivResultado.append(paragrafoResultado);
+        paragrafoResultado.innerHTML = `<p>O maior valor informado foi ${maior}.</p>`
+        domElementoDivResultado.appendChild(paragrafoResultado);
 
-    paragrafoResultado.append(`Somando todos os valores, temos ${soma}.`);
-    domElementoDivResultado.append(paragrafoResultado);
+        paragrafoResultado.innerHTML = `<p>O menor valor informado foi ${menor}.</p>`
+        domElementoDivResultado.appendChild(paragrafoResultado);
 
-    paragrafoResultado.append(`A média dos valores digitados é ${media}.`);
-    domElementoDivResultado.append(paragrafoResultado);    
+        paragrafoResultado.innerHTML = `<p>Somando todos os valores, temos ${soma}.</p>`
+        domElementoDivResultado.appendChild(paragrafoResultado);
+
+        paragrafoResultado.innerHTML = `<p>A média dos valores digitados é ${media}.</p>`
+        domElementoDivResultado.appendChild(paragrafoResultado);
+
+     
+    }
     
 }
